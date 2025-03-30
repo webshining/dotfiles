3c80c7e78176e78528e3063b250afabbad447d67
webshining <webshining@protonmail.com>
2025-03-30 21:37:16 +0000

    updated

--- configs/polybar/scripts/battery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-BATTERY_LEVEL=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-
-echo "$BATTERY_LEVEL"
--- configs/polybar/scripts/nvidia-temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
-printf "%2d" $temp
